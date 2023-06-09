b7960cbc0b32232498296efbdb9ab2899199a2be
Ing-Aviles <roberavileskt@gmail.com>
2023-06-08 23:20:01 -0600

    home

---
+++ src/JumpthePumpkin/JumpPump.java
@@ -0,0 +1,11 @@
+package JumpthePumpkin;
+
+public class JumpPump {
+
+    public JumpPump(){
+        System.out.println("Jump Pump");
+    }
+    public static void main(String[] args) throws Exception {
+        JumpPump jumpPump = new JumpPump();
+    }
+}
